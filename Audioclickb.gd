2426812e926243352a76e512bea81071ad6cb99b
Бендер <andrucom123@gmail.com>
2023-09-09 12:26:08 +0300

    Update
    - Add sound for buttons

---
+++ Audioclickb.gd
@@ -0,0 +1,22 @@
+extends AudioStreamPlayer2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func _on_button_restart_pressed():
+	self.play()
+	pass # Replace with function body.
+
+
+func _on_button_pressed():
+	self.play()
+	pass # Replace with function body.
--- Panel.gd
+++ Panel.gd
@@ -1,17 +1,8 @@
 extends Panel
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func _input(event):
 	if event.is_action_pressed("Click"):
-		# $AudioClick.play()
+		$AudioClick.play()
 		pass
 
