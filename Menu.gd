10b2cba466c2ebf97d9e01ef4d5885c9af496002
Бендер <andrucom123@gmail.com>
2023-09-09 14:57:16 +0300

    Update
    - Add menu (test)

--- Menu.gd
+++ Menu.gd
@@ -3,19 +3,9 @@ extends Panel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.visible = false
-	pass
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
 	pass
-
-func _input(event):
-	if event.is_action_pressed("menu"):
-		if  self.visible == true:
-			self.visible = false
-		else:
-			self.visible = true
-		pass
-
